805757ed820c92da5fcb9a2c3df673251982f20c
Oolong <khanh.n2005@gmail.com>
2025-11-08 11:15:34 +0700

    Xem Chi Tiết

---
+++ QuanLyBanGiay/Controllers/SanPhamController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuanLyBanGiay.Models;
+using System.Linq;
+
+namespace QuanLyBanGiay.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private readonly QL_GiayContext db = new QL_GiayContext();
+
+        public IActionResult Chitiet(int id)
+        {
+            // Lấy sản phẩm theo ID và include các liên kết
+            var sp = db.Sanphams
+                .Include(x => x.MadmNavigation)
+                .Include(x => x.MathNavigation)
+                .Include(x => x.ChitietSanphams)
+                    .ThenInclude(ct => ct.MamauNavigation)
+                .Include(x => x.ChitietSanphams)
+                    .ThenInclude(ct => ct.MasizeNavigation)
+                .FirstOrDefault(x => x.Masp == id);
+
+            // Nếu không tìm thấy sản phẩm
+            if (sp == null)
+            {
+                return RedirectToAction("Index", "Home"); 
+            }
+
+            // Truyền dữ liệu sang View
+            return View(sp);
+        }
+    }
+}
