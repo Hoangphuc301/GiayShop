13d1b1779791264a9631493ea76882b9e9c3ed74
Hoangphuc301 <nguyenphuc8794@gmail.com>
2025-11-09 16:08:39 +0700

    Tuần 3 lần 2

--- QuanLyBanGiay/obj/Debug/net6.0/QuanLyBanGiay.AssemblyInfo.cs
+++ QuanLyBanGiay/obj/Debug/net6.0/QuanLyBanGiay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyBanGiay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7004234e70f72d28e1a85daaea3297f324bfb13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyBanGiay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyBanGiay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
